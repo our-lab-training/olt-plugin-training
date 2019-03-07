b2a459e7b751d432288e5a7268885e0b9dae06c8
Fraser Bullock <mrfrase3@gmail.com>
2019-03-07 18:29:29 +0800

    fix adding inductors

--- api/services/inductions/inductions.perms.js
+++ api/services/inductions/inductions.perms.js
@@ -3,7 +3,7 @@ const checkPerm = require('../../../../../lib/checkPerm');
 module.exports = (app) => {
   app.perms.addPermListener('inductions.*.inductor', async (context, perm) => {
     const { params } = context;
-    const parts = perm.split('.');
+    const parts = typeof perm === 'string' ? perm.split('.') : perm;
     const induct = await app.service('inductions').get(parts[1]);
     if (checkPerm(`${induct.groupId}.inductions.write`, params.user)) return true;
     if (checkPerm(`${induct.groupId}.users.write`, params.user)) return true;
