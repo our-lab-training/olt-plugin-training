2cde1b66f90f30cfb96f6b67e4478d0195c018ee
mrfrase3 <mrfrase3@gmail.com>
2019-06-16 17:43:54 +0800

    restrict backend

--- api/services/inductions/inductions.hooks.js
+++ api/services/inductions/inductions.hooks.js
@@ -2,16 +2,30 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const safeRemove = require('../../../../../hooks/safe-remove');
 const restrictMethod = require('../../../../../hooks/restrict-method');
+const filterByGroup = require('../../../../../hooks/filter-by-group');
 
 module.exports = {
   before: {
     all: [authenticate('jwt')],
-    find: [],
-    get: [],
-    create: [restrictMethod(['{data.groupId}.inductions.write'])],
-    update: [restrictMethod(['{existing.groupId}.inductions.write'])],
-    patch: [restrictMethod(['{existing.groupId}.inductions.write'])],
-    remove: [restrictMethod(['{existing.groupId}.inductions.write']), safeRemove()]
+    find: [
+      filterByGroup({ perms: '{groupId}.inductions.read', override: 'superadmin.groups.read' }),
+    ],
+    get: [
+      filterByGroup({ perms: '{groupId}.inductions.read', override: 'superadmin.groups.read' }),
+    ],
+    create: [
+      restrictMethod(['{data.groupId}.inductions.write', '{data.groupId}.inductions.suggest']),
+    ],
+    update: [
+      restrictMethod(['{existing.groupId}.inductions.write', '{existing.groupId}.inductions.suggest']),
+    ],
+    patch: [
+      restrictMethod(['{existing.groupId}.inductions.write', '{existing.groupId}.inductions.suggest']),
+    ],
+    remove: [
+      restrictMethod(['{existing.groupId}.inductions.write']),
+      safeRemove(),
+    ]
   },
 
   after: {
--- api/services/trainings/trainings.hooks.js
+++ api/services/trainings/trainings.hooks.js
@@ -1,16 +1,31 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const safeRemove = require('../../../../../hooks/safe-remove');
 const checkComplete = require('../../hooks/checkComplete');
+const restrictMethod = require('../../../../../hooks/restrict-method');
+const filterByGroup = require('../../../../../hooks/filter-by-group');
 
 module.exports = {
   before: {
     all: [authenticate('jwt')],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [safeRemove()]
+    find: [
+      filterByGroup({ perms: '{groupId}.trainings.read', override: 'superadmin.groups.read' }),
+    ],
+    get: [
+      filterByGroup({ perms: '{groupId}.trainings.read', override: 'superadmin.groups.read' }),
+    ],
+    create: [
+      restrictMethod(['{data.groupId}.trainings.write', '{data.groupId}.trainings.suggest']),
+    ],
+    update: [
+      restrictMethod(['{existing.groupId}.trainings.write', '{existing.groupId}.trainings.suggest']),
+    ],
+    patch: [
+      restrictMethod(['{existing.groupId}.trainings.write', '{existing.groupId}.trainings.suggest']),
+    ],
+    remove: [
+      restrictMethod(['{existing.groupId}.trainings.write']),
+      safeRemove(),
+    ]
   },
 
   after: {
