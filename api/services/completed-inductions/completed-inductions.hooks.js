4130c4a894a44d80ee9378d93b075adcad7dd1e9
mrfrase3 <mrfrase3@gmail.com>
2019-06-16 18:27:25 +0800

    restrict completed inductions

--- api/services/completed-inductions/completed-inductions.hooks.js
+++ api/services/completed-inductions/completed-inductions.hooks.js
@@ -1,18 +1,42 @@
 
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { iff, disallow, discard } = require('feathers-hooks-common');
+const _ = require('lodash');
 const restrictMethod = require('../../../../../hooks/restrict-method');
 const safeRemove = require('../../../../../hooks/safe-remove');
+const filterByGroup = require('../../../../../hooks/filter-by-group');
+const comparePerm = require('../../../../../lib/comparePerm');
 const validateUsers = require('../../hooks/validate-users');
 const addInductionPerm = require('../../hooks/add-induction-perm');
+
+const filterByAccess = ctx => {
+  if (!ctx.params.query.$and || !ctx.params.user) return ctx;
+  const groups = ctx.params.query.$and.pop();
+  const inductIds = _.uniq(ctx.params.user.perms.all.reduce((a, perm) => {
+    const match = comparePerm('inductions.*.inductor', perm, false);
+    return match ? [...a, perm[1]] : a;
+  }, [])).filter(id => /[0-9abcdef]{24}/.test(id));
+  const or = [
+    groups,
+    { userIds: ctx.params.user._id },
+    { inductId: { $in: inductIds } },
+  ];
+  ctx.params.query.$and.push({$or: or});
+};
 
 module.exports = {
   before: {
     all: [authenticate('jwt')],
-    find: [],
-    get: [],
+    find: [
+      filterByGroup({ perms: ['{groupId}.inductions.write', '{groupId}.inductions.inductor'], override: 'superadmin.groups.read' }),
+      filterByAccess,
+    ],
+    get: [
+      filterByGroup({ perms: ['{groupId}.inductions.write', '{groupId}.inductions.inductor'], override: 'superadmin.groups.read' }),
+      filterByAccess,
+    ],
     create: [
-      restrictMethod(['inductions.{data.inductId}.inductor', '{data.groupId}.inductions.inductor']),
+      restrictMethod(['inductions.{data.inductId}.inductor', '{data.groupId}.inductions.inductor', '{data.groupId}.inductions.write']),
       discard('userIds'),
       validateUsers(),
     ],
